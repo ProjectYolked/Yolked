96466dc0f2a43f226a3de7590e2362568220d3f7
Arshon Saadati <Arshon.Saadati@skyryse.com>
2023-12-08 17:15:05 -0800

    Added React componnent to begin testing routes

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserFetcher from './components/UserFetcher';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to YOLKKED</h1>
+            <UserFetcher />
+        </div>
+    );
+}
+
+export default App;
--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../models/user'); // Import the User model
 // Controller function to get a user by ID
 exports.getUserById = async (req, res) => {
     try {
-        const user = await User.findById(req.params.id).populate('programs');
+        const user = await User.findOne({username: req.params.id}).populate('programs');
 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
