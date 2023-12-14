d14f6b68ed3ef63993ee09844086942c6727baa8
seth174 <sethfagen22@gmail.com>
2023-12-13 20:54:13 -0800

    Added winston for logging

--- server/server.js
+++ server/server.js
@@ -4,15 +4,21 @@ const workoutProgramRoutes = require('./api/routes/workoutProgramRoutes');
 const workoutRoutes = require('./api/routes/workoutRoutes');
 const userRoutes = require('./api/routes/userRoutes');
 const connectDB = require('./config/db');
+const morgan = require('morgan');
+const logger = require('./config/logger');
 
 // Connect to Database
 const startServer = async () => {
     try {
         await connectDB();
-        console.log('Database connected successfully');
+        logger.info('Database connected successfully');
 
         const app = express();
         app.use(cors());
+
+        // Use Morgan for HTTP request logging
+        // Found normal use annoying
+        //app.use(morgan('combined'));
 
         app.use(express.json()); // Needed to parse JSON bodies
 
@@ -21,9 +27,9 @@ const startServer = async () => {
         app.use('/api', workoutRoutes);
 
         const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
     } catch (error) {
-        console.error('Database connection failed', error);
+        logger.error('Database connection failed', error);
         process.exit(1); // Exit with failure
     }
 };
